d3811bd5db50526c9fb22e1c882b9c27a3560646
akim-dev <musta.akim@gmail.com>
2022-06-14 07:28:10 +0700

    2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log('hello!')
+// console.log('dajdiajdiajdisajd!')
+const calculator = {
+  displayNumber: '0',
+  operator: null,
+  firstNumber: null,
+  waitingForNumber: false,
+};
+
+function updateDisplay() {
+  document.querySelector('#displayNumber').innerHTML = calculator.displayNumber;
+}
+
+function clearCalculator() {
+  calculator.displayNumber = '0';
+  calculator.operator = null;
+  calculator.firstNumber = null;
+  calculator.waitingForNumber = false;
+}
+
+function inputDigit(digit) {
+  if (calculator.displayNumber === '0') {
+    calculator.displayNumber = digit;
+  } else {
+    calculator.displayNumber += digit;
+  }
+}
+
+const buttons = document.querySelectorAll('.button');
+for (let button of buttons) {
+  button.addEventListener('click', function (e) {
+    const target = event.target;
+
+    if (target.classList.contains('clear')) {
+      clearCalculator();
+      updateDisplay();
+      return;
+    }
+
+    inputDigit(target.innerText);
+    updateDisplay();
+  });
+}
